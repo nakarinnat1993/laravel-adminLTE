44ce5c53da3fe1c6a90eaf24a3c479d9ca5ba20b
nakarin <nakarinnat1993@gmail.com>
2020-06-11 17:35:01 +0700

    artisan ui bootstrap --auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
+            $table->string('is_admin');
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/blank', function () {
     return view('backend.layouts.app');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
