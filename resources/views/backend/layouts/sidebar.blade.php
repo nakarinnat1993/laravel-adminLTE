991851091b2aecaa21b45c247e480b73dec9c5fa
nakarin <nakarinnat1993@gmail.com>
2020-06-16 16:46:24 +0700

    Separate nav

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -1,3 +1,8 @@
+<a href="/backend" class="brand-link">
+    <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+        class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">{{ config('app.name', 'Laravel') }}</span>
+</a>
 <div class="sidebar">
     <!-- Sidebar user (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
